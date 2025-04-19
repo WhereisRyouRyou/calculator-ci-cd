662b99ba7de027f00e9eba8b1e0f8f98aca69d60
WhereisRyouRyou <115520465+WhereisRyouRyou@users.noreply.github.com>
2025-04-19 17:16:22 +0800

    Update app logic and pipeline config

--- test_app.py
+++ test_app.py
@@ -1,3 +1,18 @@
-import app
-def test_add():
-    assert app.add(2, 3) == 5
+import unittest
+from app import app
+
+class CalculatorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_addition(self):
+        response = self.client.get('/calc?a=5&b=3&op=add')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json()['result'], 8)
+
+    def test_invalid_operation(self):
+        response = self.client.get('/calc?a=5&b=3&op=xyz')
+        self.assertEqual(response.status_code, 400)
+
+if __name__ == '__main__':
+    unittest.main()
